7486d68784d2f106f1584d759b272b2f01f7aa08
Rihab BEJI <rihabbeji@yahoo.fr>
2021-11-11 14:50:45 +0100

    Update JewelleryRESTController.java

--- src/main/java/com/airtable/perfect/one/controller/JewelleryRESTController.java
+++ src/main/java/com/airtable/perfect/one/controller/JewelleryRESTController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import com.airtable.perfect.one.beans.Jewellery;
 import com.airtable.perfect.one.service.IJewelleryService;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class JewelleryRESTController
 
 {
